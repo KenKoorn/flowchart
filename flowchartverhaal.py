5d37a4bbe3629f7bf4e7446961bb92dd94e53390
KenKoorn <115067945+KenKoorn@users.noreply.github.com>
2022-10-26 11:34:32 +0200

    Add files via upload

---
+++ flowchartverhaal.py
@@ -0,0 +1,61 @@
+print("Er is Oorlog in Oekraine.")
+print("Je komt te plotseling te horen dat in je moederland oorlog is, je woont met je familie maar.")
+print("Je moet snel keuzes maken, kies de mogelijke keuzes. Keuze 1 Vluchten of keuze 2 Blijven..")
+choice = input()
+if choice == "1":
+    print("Jij en je familie vluchten.")
+    print("Er is 500km file, je bent aan het twijfelen.Wat ga je doen? Keuze 1, verder gaan. Keuze 2 Terug gaan.")
+    
+    choice = input()
+    if choice == "1":
+        print("Je gaat verder met je reis.")
+        print("Jullie zijn eindelijk uit de file en alleen. Jullie hadden pauze genomen en gaan weer door. Na uren zijn jullie vervolgens dichtbij de border van Poland.")
+        print("Helaas, krijg je zojuist te horen dat je niet mag vluchten en moet vechten voor je land..")
+        print("Wat ga je doen? Keuze 1, Terug naar je huis of 2 Vaarwel zeggen tegen je familie en mee met de soldaten.")
+        choice = input()
+        if choice == "1":
+            print("Je gaat met je familie terug naar jullie huis.")
+            print("Je bent na dagen weer thuis. En alles van je stad is stuk..")
+            print("Jullie gaan jullie huis in en niets is veranderd.")
+            print("Jullie nemen onderdak....")
+            print("Na een jaar onderdak genomen te hebben, komen de russen erachter. Ze schieten jullie dood.. Einde 1.")
+        elif choice == "2":
+            print("Vaarwel zeggen tegen je fam, en mee gaan met de soldaten.")
+            print("Ze trainen je voor weken lang. En je moet naar de frontline.")
+            print("Je wordt in je eerste missie in de neck geschoten, je bloed uit en je weet dat je dood gaat.")
+            print("Je gaat dood, maar je weet dat je familie gelukkig veilig is. Einde 2")
+
+    elif choice == "2":
+     print("Jij en je familie blijven.")
+     print("Je hoort veel bomben en je bent bang.")
+     print("Je krijgt te horend dat soldaten je stad binnen vallen.")
+     print("Je bent thuis, en denkt dat je veilig ")
+     print("Soldaten vallen je huis binnen. Wat doe je? Keuze 1, je vecht terug met een revolver. Keuze 2 je geeft over.")
+    choice = input()
+    if choice == "1":
+        print("Je mist alles en je familie is neergeschoten..lol")
+        print("De russen nemen je mee en zeggen dat je moet luisteren. Anders ben je dood.")
+        print("Je luisterd en ze willen dat je mee gaat vechten met de russen..")
+        print("Je hebt geen keus en gaat mee.")
+        print("Je veraard je moederland, en vecht tegen je eigenland.")
+        print("Je vecht in je eigen stad en de russen hebben veel mensen hun leven bespaard.")
+        print("Je mist je familie en je neemt een kijkje naar je oude huis.")
+        print("Je staat bij de deur en ziet je familie...")
+        print("Je bent verbaast en blij.")
+        print("Je bent stiekem van plan om te vluchten van de russen met je familie.")
+        print("Twee soldaten zien je praten met je familie en lopen je af. Wat doe je? Keuze 1 Je wilt met je familie mee, maar de russen willen dat niet. Keuze 2 Je schiet ze beide dood, en vlucht met je familie naar Nederland.")
+        
+        choice = input()
+        if choice == "1":
+            print("De russen vinden dat je moeilijk doen en schieten je dood.... einde 4")
+        elif choice == "2":
+                print("Je schiet ze beide dood, en vlucht met je familie naar Nederland.")
+                print("Je bent in Nederland en bent een kapper en heb een geluk leven met je familie, goeie einde 5")
+                
+
+
+    elif choice == "2":
+        print("Je geeft je over.")
+        print("Je familie wordt apart mee genomen.")
+        print("Je vecht de soldaten af, omdat ze je familie meenemen")
+        print("De russen vinden dat je moeilijk doen en schieten je dood....Einde 3")
